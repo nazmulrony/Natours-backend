7497b4c30f2acc2037cec24c2407d1e27e49f18d
nazmulrony <nhrony65@gmail.com>
2023-12-25 22:49:42 +0600

    get tours request added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs')
 
 const app = express();
 
@@ -9,6 +10,16 @@ app.get('/', (req, res) => {
 
 })
 
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: { tours }
+    })
+
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}...`);
