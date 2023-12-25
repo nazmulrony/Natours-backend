42dd8edbe1ed75ecf7b3648c652e463675378390
nazmulrony <nhrony65@gmail.com>
2023-12-25 23:24:11 +0600

    post tour req added

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const fs = require('fs')
 
 const app = express();
 
+//simple middleware
+app.use(express.json())
+
 const port = 3000;
 
 app.get('/', (req, res) => {
@@ -12,11 +15,30 @@ app.get('/', (req, res) => {
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
+//get tours
 app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
         status: 'success',
         results: tours.length,
         data: { tours }
+    })
+
+})
+
+//post tours
+app.post('/api/v1/tours', (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body)
+
+    tours.push(newTour)
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
     })
 
 })
