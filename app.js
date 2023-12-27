e4270bf47f09ba8981efedbaaca67eba60d925fd
nazmulrony <nhrony65@gmail.com>
2023-12-27 23:08:55 +0600

    env variables added

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const fs = require('fs');
 const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -8,7 +11,9 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 //MIDDLEWARES
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const app = require('./app');
 
 const port = 3000;
 
+// console.log(process.env);
+
 // START THE SERVER
 app.listen(port, () => {
     console.log(`Server running on port ${port}...`);
