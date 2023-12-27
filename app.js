98a9c6382a76ce8290e316d561a547517b546d45
nazmulrony <nhrony65@gmail.com>
2023-12-27 22:41:20 +0600

    static file middleware added

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = express();
 //MIDDLEWARES
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
     console.log('Hello form the middleware!👀');
