5a8a76173bc08a8d65ab0d44b39d673cf8ba6744
nazmulrony <nhrony65@gmail.com>
2024-01-06 22:39:18 +0600

    mongoose connected

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const app = require('./app');
 
 dotenv.config();
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose
+    .connect(DB, {
+        //these are some options to deal with deprecation warnings
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    })
+    .then(() => console.log('DB connection successful'));
 
 const port = 3000;
 
