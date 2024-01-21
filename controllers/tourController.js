56d6e03e11dbf8ca735acaf69cf03c3d974c423e
nazmulrony <nhrony65@gmail.com>
2024-01-21 22:44:39 +0600

    update tour api updated

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -57,11 +57,22 @@ exports.getTour = async (req, res) => {
     }
 };
 
-exports.updateTour = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        data: '<Updated Tour here>'
-    });
+exports.updateTour = async (req, res) => {
+    try {
+        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        res.status(200).json({
+            status: 'success',
+            data: { tour }
+        });
+    } catch (error) {
+        res.status(404).json({
+            status: 'fail',
+            message: error
+        });
+    }
 };
 
 exports.deleteTour = (req, res) => {
@@ -73,6 +84,7 @@ exports.deleteTour = (req, res) => {
 
 exports.createTour = async (req, res) => {
     try {
+        //same as const newTour = new Tour({}); newTour.save()
         const newTour = await Tour.create(req.body);
 
         res.status(201).json({
