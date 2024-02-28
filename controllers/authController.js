53ba5d984e8083db3a8427cf06d8a1d1f741d355
nazmulrony <nhrony65@gmail.com>
2024-02-28 23:31:40 +0600

    sending token to cookie is implemented

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,6 +15,19 @@ const signToken = id => {
 const createSendToken = (user, statuscode, res) => {
     const token = signToken(user._id);
 
+    const cookieOptions = {
+        expires: new Date(
+            Date.now() + process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+        ),
+
+        httpOnly: true
+    };
+
+    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+    res.cookie('jwt', token, cookieOptions);
+
+    user.password = undefined;
     res.status(statuscode).json({
         status: 'success',
         token,
